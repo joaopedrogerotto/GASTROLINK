952acafe26f28fccfbf243f2f8b61cb5e2b0e2bd
jpgerotto <jpgertto.1204fernadesz@gmail.com>
2026-04-25 12:38:17 -0300

    refactor: refatoração para o tipo de usuario gravado seja a string em vez de id, evitando de ser fixo no id no banco de dados

--- GastroLink/Controllers/LoginController.cs
+++ GastroLink/Controllers/LoginController.cs
@@ -27,6 +27,7 @@ namespace GastroLink.Controllers {
 
             HttpContext.Session.SetString("NomeUsuario", usuario.Nome);
             HttpContext.Session.SetInt32("IdTipoUsuario", usuario.Tipo.Id);
+            HttpContext.Session.SetString("TipoUsuarioStr", usuario.Tipo.Tipo);
 
             return RedirectToAction("TodasMesas","Mesa");
         }
