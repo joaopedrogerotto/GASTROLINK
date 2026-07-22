ec1ce47ed60dc0af97230095b99c9425501c3a1f
jpgerotto <jpgertto.1204fernadesz@gmail.com>
2026-07-22 09:23:36 -0300

    feat: atualização de pedido

--- GastroLink/obj/Debug/net9.0/GastroLink.AssemblyInfo.cs
+++ GastroLink/obj/Debug/net9.0/GastroLink.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GastroLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c0ee1d3d46dc19a3f555bab95db26057a9b0c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f158eaabee11392e012e3e8de4b629ffe8d562")]
 [assembly: System.Reflection.AssemblyProductAttribute("GastroLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GastroLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GastroLink/wwwroot/js/cozinha.js
+++ GastroLink/wwwroot/js/cozinha.js
@@ -14,14 +14,26 @@ function adicionarNovoPedidoNaTela(pedido) {
         <p class="card-text">Observação: ${item.observacao}</p>
         <hr>
     `).join("");
-    container?.insertAdjacentHTML("beforeend", `<div class="card">
-            <div class="card-header">
-                Pedido #${pedido.id}
+    const data = new Date(pedido.dataCriacao);
+    const dataFormatada = data.toLocaleString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false
+    });
+    container?.insertAdjacentHTML("beforeend", `<div class="card card-pedido">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <span>Pedido #${pedido.id}</span>
+                <small class="text-muted">
+                    ${dataFormatada}
+                </small>
             </div>
             <div class="card-body">
                 <h5 class="card-title">Mesa ${pedido.mesa.numero}</h5>
                 ${itensPedido}
-                <a href="#" class="btn btn-primary">PRONTO</a>
+                 <button href="#" class="btn btn-info" data-id-pedido="${pedido.id}" data-id-status="3" id="btnPedidoPronto">EM PREPARO</button>
             </div>
         </div>
         <br>`);
@@ -31,14 +43,25 @@ document.addEventListener("click", (e) => {
     if (!btnPedidoPronto) {
         return;
     }
-    let statusPedido = { IdPedido: Number(btnPedidoPronto.dataset.idPedido), IdStatusPedido: 4 };
+    const carPedido = btnPedidoPronto.closest(".card-pedido");
+    let idStatus = Number(btnPedidoPronto.dataset.idStatus);
+    let idPedido = Number(btnPedidoPronto.dataset.idPedido);
+    let statusPedido = { IdPedido: idPedido, IdStatusPedido: idStatus };
     $.ajax({
         url: '/Pedido/AtualizarStatusPedido',
         method: 'POST',
         data: JSON.stringify(statusPedido),
         contentType: 'application/json',
         success: function () {
-            location.reload();
+            if (idStatus === 4) {
+                carPedido?.remove();
+            }
+            else if (idStatus === 3) {
+                btnPedidoPronto.textContent = "PRONTO";
+                btnPedidoPronto.classList.remove("btn-info");
+                btnPedidoPronto.classList.add("btn-success");
+                btnPedidoPronto.dataset.idStatus = "4";
+            }
         },
         error: function (xhr, status, error) {
             console.log("Erro:" + error);
